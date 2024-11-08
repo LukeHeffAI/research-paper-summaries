0e9702430651c2faa7c9921ce78aa8d008b7b324
LukeHeffAI <125118901+LukeHeffAI@users.noreply.github.com>
2024-11-08 14:52:34 +1030

    Refactor to use dataclasses rather that static variables

--- research_details.py
+++ research_details.py
@@ -1,4 +1,12 @@
 from dataclasses import dataclass
+import json
+
+with open("research_data.json", "r") as file:
+    research_data = json.load(file)
+
+# TODO:
+with open("document_data.json", "r") as file:
+    document_data = json.load(file)
 
 @dataclass
 class ResearchContext:
@@ -6,10 +14,11 @@ class ResearchContext:
     research_topic: str
     research_context: str
     research_focus: str
-    document_type: str
-    document_return_details: str
     document_intro: str
 
+class DocumentContext:
+    document_type: str
+    document_return_details: str
 
 def vowel_check(text):
     return "an " + text if text[0].lower() in "aeiou" else "a " + text
@@ -18,12 +27,14 @@ def build_context_prompt(user_context: ResearchContext):
     # Research details
     research_field = "Anthropology"
     research_topic = "Indigenous studies in Australia"
-    research_context = f"The general context of my research is in {research_topic}, with a focus on land based education and building education programs that incorporate indigenous knowledges and pedagogies."
+    # TODO: Build an input field to take these individual points and turn them into a list of this structure
     research_focus = """
     - Learning on-country principles 
-    - Meaning of 'Country' 
+    - Meaning of 'Country' in indigenous contexts
     - Settler colonial theory 
     - Land education/pedagogy and curriculum development"""
+    # TODO: Find a way to make the following more dynamic, changing based on the user's input. Potentially use an OpenAI model to generate the text after the comma based on "research_focus".
+    research_interests = f"The general interest in my research is in {research_topic}, with a focus on land based education and building education programs that incorporate indigenous knowledges and pedagogies."
     research_focus_structure = f"The focus points of my research can be summarised as:{research_focus}"
 
     # Document details
@@ -39,6 +50,6 @@ def build_context_prompt(user_context: ResearchContext):
     document_intro = "The following text is from a PDF of the document I would like to summarise:"
 
     # Overall prompt built from the above details
-    prompt = f"{document_prompt_structure}\n\n{research_context} {research_focus_structure}\n\n{document_intro}"
+    prompt = f"{document_prompt_structure}\n\n{research_interests} {research_focus_structure}\n\n{document_intro}"
 
     return prompt
